a394e906b9377191953ed4e690a11d7a7c9d44de
thepresidentm <miguelanva9@gmail.com>
2022-07-21 00:51:31 -0500

    ing aeoroespacial

---
+++ 2021/ing-aeroespacial/json.js
@@ -0,0 +1,490 @@
+const json = [
+    {
+        "puntaje": 0,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 1,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 2,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 3,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 4,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 5,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 6,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 7,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 8,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 9,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 10,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 11,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 12,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 13,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 14,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 15,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 16,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 17,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 18,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 19,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 20,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 21,
+        "cantidad": 1
+    },
+    {
+        "puntaje": 22,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 23,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 24,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 25,
+        "cantidad": 1
+    },
+    {
+        "puntaje": 26,
+        "cantidad": 1
+    },
+    {
+        "puntaje": 27,
+        "cantidad": 1
+    },
+    {
+        "puntaje": 28,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 29,
+        "cantidad": 2
+    },
+    {
+        "puntaje": 30,
+        "cantidad": 6
+    },
+    {
+        "puntaje": 31,
+        "cantidad": 5
+    },
+    {
+        "puntaje": 32,
+        "cantidad": 9
+    },
+    {
+        "puntaje": 33,
+        "cantidad": 5
+    },
+    {
+        "puntaje": 34,
+        "cantidad": 11
+    },
+    {
+        "puntaje": 35,
+        "cantidad": 7
+    },
+    {
+        "puntaje": 36,
+        "cantidad": 7
+    },
+    {
+        "puntaje": 37,
+        "cantidad": 9
+    },
+    {
+        "puntaje": 38,
+        "cantidad": 13
+    },
+    {
+        "puntaje": 39,
+        "cantidad": 9
+    },
+    {
+        "puntaje": 40,
+        "cantidad": 14
+    },
+    {
+        "puntaje": 41,
+        "cantidad": 14
+    },
+    {
+        "puntaje": 42,
+        "cantidad": 21
+    },
+    {
+        "puntaje": 43,
+        "cantidad": 15
+    },
+    {
+        "puntaje": 44,
+        "cantidad": 21
+    },
+    {
+        "puntaje": 45,
+        "cantidad": 20
+    },
+    {
+        "puntaje": 46,
+        "cantidad": 18
+    },
+    {
+        "puntaje": 47,
+        "cantidad": 19
+    },
+    {
+        "puntaje": 48,
+        "cantidad": 14
+    },
+    {
+        "puntaje": 49,
+        "cantidad": 18
+    },
+    {
+        "puntaje": 50,
+        "cantidad": 20
+    },
+    {
+        "puntaje": 51,
+        "cantidad": 25
+    },
+    {
+        "puntaje": 52,
+        "cantidad": 18
+    },
+    {
+        "puntaje": 53,
+        "cantidad": 30
+    },
+    {
+        "puntaje": 54,
+        "cantidad": 26
+    },
+    {
+        "puntaje": 55,
+        "cantidad": 14
+    },
+    {
+        "puntaje": 56,
+        "cantidad": 16
+    },
+    {
+        "puntaje": 57,
+        "cantidad": 16
+    },
+    {
+        "puntaje": 58,
+        "cantidad": 14
+    },
+    {
+        "puntaje": 59,
+        "cantidad": 26
+    },
+    {
+        "puntaje": 60,
+        "cantidad": 18
+    },
+    {
+        "puntaje": 61,
+        "cantidad": 23
+    },
+    {
+        "puntaje": 62,
+        "cantidad": 20
+    },
+    {
+        "puntaje": 63,
+        "cantidad": 25
+    },
+    {
+        "puntaje": 64,
+        "cantidad": 31
+    },
+    {
+        "puntaje": 65,
+        "cantidad": 28
+    },
+    {
+        "puntaje": 66,
+        "cantidad": 17
+    },
+    {
+        "puntaje": 67,
+        "cantidad": 21
+    },
+    {
+        "puntaje": 68,
+        "cantidad": 21
+    },
+    {
+        "puntaje": 69,
+        "cantidad": 22
+    },
+    {
+        "puntaje": 70,
+        "cantidad": 10
+    },
+    {
+        "puntaje": 71,
+        "cantidad": 19
+    },
+    {
+        "puntaje": 72,
+        "cantidad": 19
+    },
+    {
+        "puntaje": 73,
+        "cantidad": 24
+    },
+    {
+        "puntaje": 74,
+        "cantidad": 16
+    },
+    {
+        "puntaje": 75,
+        "cantidad": 18
+    },
+    {
+        "puntaje": 76,
+        "cantidad": 19
+    },
+    {
+        "puntaje": 77,
+        "cantidad": 21
+    },
+    {
+        "puntaje": 78,
+        "cantidad": 19
+    },
+    {
+        "puntaje": 79,
+        "cantidad": 19
+    },
+    {
+        "puntaje": 80,
+        "cantidad": 15
+    },
+    {
+        "puntaje": 81,
+        "cantidad": 14
+    },
+    {
+        "puntaje": 82,
+        "cantidad": 18
+    },
+    {
+        "puntaje": 83,
+        "cantidad": 15
+    },
+    {
+        "puntaje": 84,
+        "cantidad": 12
+    },
+    {
+        "puntaje": 85,
+        "cantidad": 10
+    },
+    {
+        "puntaje": 86,
+        "cantidad": 13
+    },
+    {
+        "puntaje": 87,
+        "cantidad": 14
+    },
+    {
+        "puntaje": 88,
+        "cantidad": 12
+    },
+    {
+        "puntaje": 89,
+        "cantidad": 12
+    },
+    {
+        "puntaje": 90,
+        "cantidad": 6
+    },
+    {
+        "puntaje": 91,
+        "cantidad": 11
+    },
+    {
+        "puntaje": 92,
+        "cantidad": 14
+    },
+    {
+        "puntaje": 93,
+        "cantidad": 11
+    },
+    {
+        "puntaje": 94,
+        "cantidad": 13
+    },
+    {
+        "puntaje": 95,
+        "cantidad": 10
+    },
+    {
+        "puntaje": 96,
+        "cantidad": 10
+    },
+    {
+        "puntaje": 97,
+        "cantidad": 14
+    },
+    {
+        "puntaje": 98,
+        "cantidad": 6
+    },
+    {
+        "puntaje": 99,
+        "cantidad": 9
+    },
+    {
+        "puntaje": 100,
+        "cantidad": 8
+    },
+    {
+        "puntaje": 101,
+        "cantidad": 10
+    },
+    {
+        "puntaje": 102,
+        "cantidad": 4
+    },
+    {
+        "puntaje": 103,
+        "cantidad": 10
+    },
+    {
+        "puntaje": 104,
+        "cantidad": 5
+    },
+    {
+        "puntaje": 105,
+        "cantidad": 7
+    },
+    {
+        "puntaje": 106,
+        "cantidad": 8
+    },
+    {
+        "puntaje": 107,
+        "cantidad": 7
+    },
+    {
+        "puntaje": 108,
+        "cantidad": 7
+    },
+    {
+        "puntaje": 109,
+        "cantidad": 6
+    },
+    {
+        "puntaje": 110,
+        "cantidad": 4
+    },
+    {
+        "puntaje": 111,
+        "cantidad": 4
+    },
+    {
+        "puntaje": 112,
+        "cantidad": 5
+    },
+    {
+        "puntaje": 113,
+        "cantidad": 7
+    },
+    {
+        "puntaje": 114,
+        "cantidad": 5
+    },
+    {
+        "puntaje": 115,
+        "cantidad": 6
+    },
+    {
+        "puntaje": 116,
+        "cantidad": 1
+    },
+    {
+        "puntaje": 117,
+        "cantidad": 1
+    },
+    {
+        "puntaje": 118,
+        "cantidad": 1
+    },
+    {
+        "puntaje": 119,
+        "cantidad": 0
+    },
+    {
+        "puntaje": 120,
+        "cantidad": 0
+    }
+];
+
+module.exports ={
+    json
+}
